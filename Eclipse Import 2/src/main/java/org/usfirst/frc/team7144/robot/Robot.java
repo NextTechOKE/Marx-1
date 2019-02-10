c5b3848842e62eebde377d88007950f5b1472acb
NextTechOKE <Jace.williams@lansing.nextechhigh.org>
2019-02-10 10:53:40 -0500

    Revert "not sure if this will work or not"
    This reverts commit 7028a6a7123b2f2bfc3cbb4193a652c9db2c0993.

--- Eclipse Import 2/src/main/java/org/usfirst/frc/team7144/robot/Robot.java
+++ Eclipse Import 2/src/main/java/org/usfirst/frc/team7144/robot/Robot.java
@@ -138,8 +138,7 @@ public class Robot extends IterativeRobot {
 		double ScalePower = 0.35;
 		
 		switch (autoSelected) {
-String R_SCALE;
-		// ALL the different Auto options are listed below--- Maybe this center will work - going right			
+// ALL the different Auto options are listed below--- Maybe this center will work - going right			
 		case R_SCALE:
 			if(gameData.charAt(0) == 'R')
 			{
@@ -349,11 +348,9 @@ String R_SCALE;
 	public void teleopPeriodic() {
 //SONIC CODY
 		drive.arcadeDrive(joystick.getRawAxis(1) * TELEOP_SPEED, joystick.getRawAxis(0) * TELEOP_SPEED );
-		Object PDPJNI;
-		// SONIC JARRETT - ARM
+//SONIC JARRETT - ARM
 		double worm_current = Math.max((PDPJNI.getPDPChannelCurrent((byte)2, 0) + PDPJNI.getPDPChannelCurrent((byte)13, 0)) / 250.0 + 0.15, 1.0);
-		System.out.println(
-				((Object) PDPJNI).getPDPChannelCurrent((byte) 2, 0) + PDPJNI.getPDPChannelCurrent((byte) 13, 0));
+		System.out.println(PDPJNI.getPDPChannelCurrent((byte)2, 0)+PDPJNI.getPDPChannelCurrent((byte)13, 0));
 		speedController2.set( 1.0 * worm_current * joystick2.getY());
 		speedController3.set(-1.0 * worm_current * joystick2.getY());
 		System.out.println(41);
